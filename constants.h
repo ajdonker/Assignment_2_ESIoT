d38fe9b0e3a8ca4cff207bab5cb6ed3bf1eb35d2
ajdonker <blagojamu03@gmail.com>
2025-11-21 18:45:36 +0100

    added all externals

---
+++ constants.h
@@ -0,0 +1,11 @@
+// Constants.h
+#if !defined(MYLIB_CONSTANTS_H)
+#define MYLIB_CONSTANTS_H 1
+
+const int T1 = 5000;
+const int T2 = 5000;
+const int T3 = 10000;
+const int T4 = 10000;
+const int TIMEOUT = 20000;
+
+#endif
